88b7896c49e4f06f7c0994670ff5a34ce85ae282
Minwoo Kang <minwoo.kang@outlook.com>
2017-10-30 13:18:46 +0900

    get-topic-file-path 위치 변경

--- src/kangfka/message_storage.clj
+++ src/kangfka/message_storage.clj
@@ -8,6 +8,13 @@
 ;topic 에서 메시지를 읽음 TODO
 (defn read-messages [topic])
 
+;topic 파일 경로를 만듬
+(defn get-topic-file-path
+  [topic]
+  (let [file-path (str (System/getProperty "user.dir") "/local-file/" topic ".kangfka")]
+    (println (str "[topic] " topic " => " file-path))
+    file-path))
+
 ;topic 파일이 없으면 생성함 TODO
 (defn create-topic-file
   [topic]
@@ -15,12 +22,5 @@
     (if (.exists (io/file topic-file-path))
       (println (str topic-file-path " already exists."))
       (println (str topic-file-path " does not exist.")))))
-
-;topic 파일 경로를 만듬
-(defn get-topic-file-path
-  [topic]
-  (let [file-path (str (System/getProperty "user.dir") "/local-file/" topic ".kangfka")]
-    (println (str "[topic] " topic " => " file-path))
-    file-path))
 
 (create-topic-file "hi")
