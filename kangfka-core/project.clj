89186da58047a2623fc72406c75eab61cf177b13
Minwoo Kang <minwoo.kang@outlook.com>
2017-12-19 13:23:02 +0900

    Add zookeeper.clj

--- kangfka-core/project.clj
+++ kangfka-core/project.clj
@@ -5,4 +5,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.json "0.2.6"]])
+                 [org.clojure/data.json "0.2.6"]
+                 [org.apache.zookeeper/zookeeper "3.4.11"]])
