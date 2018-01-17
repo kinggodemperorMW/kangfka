84b9a9698d679475cfe82e311fc0566145838fa2
Beomseok Kim <beomseok.kim@worksmobile.com>
2018-01-17 13:23:08 +0900

    테스트 오류 수정

--- kangfka-core/test/kangfka_core/writer/file_writer_test.clj
+++ kangfka-core/test/kangfka_core/writer/file_writer_test.clj
@@ -13,18 +13,32 @@
 (defn create-file-path [topic partition]
       (str conf/root-topic-path "/" topic "/" partition))
 
+(defn create-write-test-file []
+    (def test-file-path (create-file-path test_topic test_partition))
+    (io/make-parents test-file-path)
+    (with-open [w (io/writer (str test-file-path) :append false)]
+        (.write w "")))
+
+(defn delete-files-recursively [fname & [silently]]
+(letfn [(delete-f [file]
+            (when (.isDirectory file)
+            (doseq [child-file (.listFiles file)]
+                (delete-f child-file)))
+            (io/delete-file file silently))]
+    (delete-f (io/file fname))))
+
 (defn remove-write-test-file [f]
-      (def test-file-path (create-file-path test_topic test_partition))
-      (if (.exists (io/file test-file-path))
-          (io/delete-file test-file-path))
-      (f))
+    (def test-file-path conf/root-topic-path)
+    (if (.exists (io/file test-file-path))
+        (delete-files-recursively test-file-path))
+    (f))
 
 (defn read-messages []
       (let [file-path (create-file-path test_topic test_partition)]
            (slurp file-path)))
 
 (deftest test-get-topic-path-when-success
-    (is (= (get-topic-path "name" 0) "/root/name/0")))
+    (is (= (get-topic-path "name" 0) "./root/name/0")))
 
 (deftest test-write-when-topic-is-empty
          (def empty_topic "")
@@ -41,9 +55,10 @@
 
 (deftest test-write-when-write-message
          (testing "메세지 한 개 파일 쓰기 성공 시나리오"
+                  (create-write-test-file)
                   (write test_topic test_partition test_message)
                   (def actual_result (read-messages))
-                  (def expected_result test_message)
+                  (def expected_result (str test_message "\n"))
                   (is (= actual_result expected_result))))
 
 (use-fixtures :each remove-write-test-file)
