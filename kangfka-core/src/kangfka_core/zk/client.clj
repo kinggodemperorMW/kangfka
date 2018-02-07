904b6790def6f9c9c17449637a020a3d72925eb7
강재석 <jaese.ok@navercorp.com>
2018-02-07 13:13:56 +0900

    add add-partition function

--- kangfka-core/src/kangfka_core/zk/client.clj
+++ kangfka-core/src/kangfka_core/zk/client.clj
@@ -3,9 +3,10 @@
   (:require [zookeeper.data :as data])
   (:require [clojure.data.json :as json]))
 
+; 외부에서 config 파일로 받아야 함
 (def connect-string "127.0.0.1:2181")
 
-; (def client (zk/connect connect-string))
+(def client (zk/connect connect-string))
 
 (defstruct node :hostname :ip :port)
 
@@ -23,7 +24,10 @@
 (defn make-dir [client path]
   (let [version (get-dir-version client path)]
    (if (nil? version)
-     (zk/create client path))))
+     (zk/create-all client path))))
+
+(defn get-nodes [client]
+  (zk/children client "/nodes"))
 
 (defn add-node [client node-list]
   (doseq [node node-list]
@@ -31,12 +35,26 @@
       (let [hostname (:hostname node)
             node-dir-name (str "/nodes/" hostname)]
         (make-dir client node-dir-name)
-        (zk/set-data client node-dir-name (data/to-bytes (json/write-str node)) (get-dir-version client node-dir-name))))))
+        (zk/set-data
+          client
+          node-dir-name
+          (data/to-bytes (json/write-str node))
+          (get-dir-version client node-dir-name))))))
+
+(defn add-partition [client topic]
+  (let [topic-name (:name topic)
+        partition-size (:partition topic)
+        node-list (get-nodes client)
+        node-list-count (count node-list)]
+    (doseq [partition-id (range 0 partition-size)]
+      (let [partition-dir-name (str "/topics/" topic-name "/" partition-id)]
+        (make-dir client partition-dir-name)
+        (zk/set-data
+         client
+         partition-dir-name
+         (.getBytes (str (nth node-list (mod partition-id node-list-count))))
+         (get-dir-version client partition-dir-name))))))
 
 (defn add-topic [client topic-list]
-  (doseq [topic topic-list]
-    (do
-      (let [topic-name (:name topic)
-            topic-dir-name (str "/topics/" topic-name)]
-        (make-dir client topic-dir-name)
-        (zk/set-data client topic-dir-name (data/to-bytes (json/write-str topic)) (get-dir-version client topic-dir-name))))))
+  (doseq [t topic-list]
+    (add-partition client t)))
